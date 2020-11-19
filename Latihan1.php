d5bc34e17bdbfae74d6dd36f1800d88aad5af39c
Inday Suci Fitriany <74606127+Indayy21@users.noreply.github.com>
2020-11-19 20:37:23 +0700

    Add files via upload

---
+++ Latihan1.php
@@ -0,0 +1,12 @@
+<?php
+
+$mahasiswa = [
+    "nama" => "Sandhika Galih",
+    "nrp" => "043040023",
+    "email" => "Sandhikagalih@unpas.ac.id"
+];
+
+$data = json_encode($mahasiswa);
+echo $data;
+
+?>
---
+++ Latihan4.php
@@ -0,0 +1,8 @@
+<?php
+
+$data = file_get_contents('Coba.json');
+$mahasiswa = json_decode($data, true);
+
+var_dump($mahasiswa);
+
+?>
